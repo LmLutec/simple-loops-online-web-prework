46ee2f30be8a097c40f051a2e050808d15739a2c
jolly-hardware-0578 <lmccoy8911@gmail.com>
2019-01-31 10:56:55 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,6 +3,7 @@
 def loop_iterator(number_of_times)
 counter = 0
  loop do 
+   counter = counter + 1 
    phrase = "Welcome to Flatiron School's Web Development Course!"
    puts phrase 
    if counter == 8
